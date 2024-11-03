61e6c500193ff8a437c7efc401b2bfafa4264507
Himanshu Kumar <himanshukeshri986@gmail.com>
2024-11-03 20:59:25 +0530

    name

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -25,7 +25,7 @@ const HomePage = () => {
               : theme.colors.gray[9],
         })}
       >
-        himanshu : ${budget - expenses}
+        Your Amount is : ${budget - expenses}
       </Text>
       <SimpleGrid cols={2} style={{ justifyContent: "center" }}>
         <DisplayCard label="Income / Budget" amount={budget} color="green.4" />
